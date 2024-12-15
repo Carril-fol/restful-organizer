d9348112def558d2b09d86020933ac4803088304
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-15 20:20:40 -0300

    chore: Remove Flask-Caching from extensions

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -1,12 +1,5 @@
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-<<<<<<< HEAD
-=======
-
-# Flask-Caching
-# https://flask-caching.readthedocs.io/en/latest/
-cache = Cache()
->>>>>>> 2d9e69f7b091b216dfb092e9f16c0d54da666969
 
 # Flask-JWT-Extended
 # https://flask-jwt-extended.readthedocs.io/en/stable/
