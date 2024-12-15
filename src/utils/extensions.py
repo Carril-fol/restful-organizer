d8de4418f6383706be68d10064efb770d14736b6
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-15 20:15:45 -0300

    chore: Updated file with all extensions

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -1,10 +1,10 @@
-from flask_caching import Cache
 from flask_jwt_extended import JWTManager
-
-# Flask-Caching
-# https://flask-caching.readthedocs.io/en/latest/
-cache = Cache()
+from flask_cors import CORS
 
 # Flask-JWT-Extended
 # https://flask-jwt-extended.readthedocs.io/en/stable/
 jwt = JWTManager()
+
+# Flask-CORS
+# https://flask-cors.readthedocs.io/en/latest/
+cors = CORS()
