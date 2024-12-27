53350e04c37bab5bf1423a7c28831b2a5548c794
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-12-27 05:59:42 -0300

    chore: Moved initialization from Flask

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -1,10 +1,22 @@
+import os
+from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+# Flask
+# https://flask.palletsprojects.com/en/stable/
+app = Flask(__name__)
+
 # Flask-JWT-Extended
 # https://flask-jwt-extended.readthedocs.io/en/stable/
-jwt = JWTManager()
+jwt = JWTManager(app)
 
 # Flask-CORS
 # https://flask-cors.readthedocs.io/en/latest/
-cors = CORS()
+cors = CORS(
+    app, 
+    supports_credentials=True, 
+    origins=["https://organizer-app-ruck.onrender.com", "http://localhost:5173"],
+    methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type", "Authorization", "X-Requested-With", "application/json"]
+)
